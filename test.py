8932bcd5b7a8ac13cd2837ebf1c1a3fd67381c87
zunzzz <a0983208130@gmail.com>
2020-09-20 21:04:13 +0800

    add comm

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 import numpy as np
-x = np.ones((4,3))
-x[0][0] = 5
-print(x)
-y = x[0:2]
-print(y.shape)
+import pickle
+# x = np.ones((4,3))
+# x[0][0] = 5
+# print(x)
+# y = x[0:2]
+# print(y.shape)
+
+msg = pickle.dumps(np.arange(12).reshape(3, 4))
+ary = pickle.loads(msg)
+print(ary, ary.shape)
